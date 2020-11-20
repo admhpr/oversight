72263b2ba786564f9f009a491802d68e752a6367
adam <adam@harpur.io>
2020-11-20 09:07:45 +0000

    chore: init

---
+++ components/Logo.vue
@@ -0,0 +1,19 @@
+<template>
+  <img src="~/assets/logo.png" alt="site logo" class="logo" />
+</template>
+
+<style lang="scss" module>
+.logo {
+  width: 200px;
+  @apply mx-auto;
+  animation: 1s appear;
+}
+@keyframes appear {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+</style>
