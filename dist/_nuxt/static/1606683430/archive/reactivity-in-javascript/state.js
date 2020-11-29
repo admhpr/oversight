a2ec1fadbadf6a654799567842f298584ef60109
adam <adam@harpur.io>
2020-11-29 20:59:15 +0000

    chore: create dist

---
+++ dist/_nuxt/static/1606683430/archive/reactivity-in-javascript/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__={staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1606683430",layout:"default",error:null,serverRendered:true,routePath:"\u002Farchive\u002Freactivity-in-javascript",config:{apiKey:"8RAPkZ1PgqvfaZPsxL6t9PBn",content:{dbHash:"dd0df1aa"}}};
