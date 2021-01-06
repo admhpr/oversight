4f6c7c9049f5a32a403cfb854bc014144d531cc2
harps116 <harps116@users.noreply.github.com>
2021-01-06 13:57:11 +0000

    Automated deployment: Wed Jan  6 13:57:11 UTC 2021 d4b8214b6c595800b6dcd390e4fd45c9002a909a

---
+++ _nuxt/static/1609941421/archive/javascript-es6-the-way-of-the-arrow-function/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__={staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1609941421",layout:"default",error:null,serverRendered:true,routePath:"\u002Farchive\u002Fjavascript-es6-the-way-of-the-arrow-function",config:{apiKey:void 0,content:{dbHash:"fa52f9c3"}}};
