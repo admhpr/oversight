402257964b22347bf50110ac2bb1b5a61d064911
admhpr <admhpr@users.noreply.github.com>
2021-04-05 14:34:29 +0000

    Automated deployment: Mon Apr  5 14:34:29 UTC 2021 7b40df4e7fb33701a47717e30b6ea4f2b10ecc7e

---
+++ _nuxt/static/1617633260/archive/javascript-es6-the-way-of-the-arrow-function/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__={staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1617633260",layout:"default",error:null,serverRendered:true,routePath:"\u002Farchive\u002Fjavascript-es6-the-way-of-the-arrow-function",config:{apiKey:void 0,content:{dbHash:"20a891a1"}}};
