70aaa5ab20ae1f70ffd28ad294df200cc00f20b8
adam <adam@harpur.io>
2021-05-04 08:43:33 +0100

    chore: update deps

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,8 +1,5 @@
 export default {
-  // Target (https://go.nuxtjs.dev/config-target)
   target: 'static',
-
-  // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     title: 'oversight',
     meta: [
@@ -27,37 +24,16 @@ export default {
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
-
-  // Global CSS (https://go.nuxtjs.dev/config-css)
   css: [],
-
-  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: ['~/plugins/axios.js', '~/plugins/posts.js'],
-
-  // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
-
-  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
   buildModules: [
-    // https://go.nuxtjs.dev/eslint
     '@nuxtjs/eslint-module',
     '@nuxtjs/style-resources',
-    // https://go.nuxtjs.dev/tailwindcss
     '@nuxtjs/tailwindcss',
   ],
-
-  // Modules (https://go.nuxtjs.dev/config-modules)
-  modules: [
-    // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
-    // https://go.nuxtjs.dev/content
-    '@nuxt/content',
-  ],
-
-  // Axios module configuration (https://go.nuxtjs.dev/config-axios)
+  modules: ['@nuxtjs/axios', '@nuxt/content'],
   axios: {},
-
-  // Content module configuration (https://go.nuxtjs.dev/config-content)
   content: {
     markdown: {
       prism: {
@@ -65,8 +41,6 @@ export default {
       },
     },
   },
-
-  // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
     extend(config, ctx) {
       if (ctx.isDev) {
