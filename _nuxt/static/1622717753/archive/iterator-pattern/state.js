e76834d0933a51864ce3ff5938eca6989a73b5ef
admhpr <admhpr@users.noreply.github.com>
2021-06-03 10:56:03 +0000

    Automated deployment: Thu Jun  3 10:56:03 UTC 2021 c433a717253e2612799038847cbfb4d679a54ae7

---
+++ _nuxt/static/1622717753/archive/iterator-pattern/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1622717753",layout:"default",error:a,serverRendered:true,routePath:"\u002Farchive\u002Fiterator-pattern",config:{apiKey:void 0,_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"c75115b8"}}}}(null));
