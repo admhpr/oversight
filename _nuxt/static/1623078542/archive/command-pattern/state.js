a49e33253047e43fdc4fa2d61817e38aa6904170
admhpr <admhpr@users.noreply.github.com>
2021-06-07 15:09:11 +0000

    Automated deployment: Mon Jun  7 15:09:11 UTC 2021 4f317eb78c307428b0727427d8671cf95b70c434

---
+++ _nuxt/static/1623078542/archive/command-pattern/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1623078542",layout:"default",error:a,serverRendered:true,routePath:"\u002Farchive\u002Fcommand-pattern",config:{apiKey:void 0,_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"741db8cf"}}}}(null));
---
+++ _nuxt/static/1623078542/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1623078542",layout:"default",error:a,serverRendered:true,routePath:b,config:{apiKey:void 0,_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"741db8cf"}}}}(null,"\u002F"));
