d6b27c1e44adbdc1f9a62a050e49332c342aba82
admhpr <admhpr@users.noreply.github.com>
2021-06-07 15:07:49 +0000

    Automated deployment: Mon Jun  7 15:07:49 UTC 2021 dfd3aa0d0b1ebc3170879532250b7b168ccdfcaa

---
+++ _nuxt/static/1623078460/blog/why-rust/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1623078460",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Fwhy-rust",config:{apiKey:void 0,_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"868612cd"}}}}(null));
