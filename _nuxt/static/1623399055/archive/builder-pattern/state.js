e4dabde33ea5ec179fddda0dacef64ff8ab8c583
admhpr <admhpr@users.noreply.github.com>
2021-06-11 10:54:45 +0000

    Automated deployment: Fri Jun 11 10:54:45 UTC 2021 e04abd61a4dd4723546e0da569302a8b02359b76

--- _nuxt/static/1623399055/archive/builder-pattern/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1623399055",layout:"default",error:a,serverRendered:true,routePath:"\u002Farchive\u002Fbuilder-pattern",config:{apiKey:void 0,_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"f5cd7724"}}}}(null));
