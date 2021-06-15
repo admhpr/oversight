9091e1e7563c2fe18c26b799c07ea8ad25a8fe12
adam <adam@harpur.io>
2021-06-15 09:24:56 +0100

    chore: add links for rehype

--- plugins/content-image.js
+++ plugins/content-image.js
@@ -1,4 +1,8 @@
-// see: https://blog.rizalrenaldi.com/blog/nuxt-content-image-inside-markdown/
+/**
+ * see: https://blog.rizalrenaldi.com/blog/nuxt-content-image-inside-markdown/
+ *      https://github.com/rehypejs/rehype/blob/main/doc/plugins.md
+ */
+
 const visit = require('unist-util-visit')
 module.exports = function nuxtContentImages() {
   return function transformer(tree, file) {
