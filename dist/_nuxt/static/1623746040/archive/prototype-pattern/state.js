cb5bd5ccaf4f9531ec8e3506bace7937c3a74650
adam <adam@harpur.io>
2021-06-16 06:40:05 +0100

    chore: remove dist

--- dist/_nuxt/static/1623746040/archive/prototype-pattern/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1623746040",layout:"default",error:a,serverRendered:true,routePath:"\u002Farchive\u002Fprototype-pattern",config:{apiKey:"8RAPkZ1PgqvfaZPsxL6t9PBn",_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"ad9c87f4"}}}}(null));
