b94493e0ae6771f1a28cce06317ad0b460987a7d
adam <adam@harpur.io>
2021-06-16 06:39:15 +0100

    chore: add svg meta

---
+++ dist/_nuxt/static/1623746040/archive/normal-distribution-taming-the-random/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1623746040",layout:"default",error:a,serverRendered:true,routePath:"\u002Farchive\u002Fnormal-distribution-taming-the-random",config:{apiKey:"8RAPkZ1PgqvfaZPsxL6t9PBn",_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"ad9c87f4"}}}}(null));
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,3 @@
-import { codePointAt } from 'core-js/core/string'
-
 export default {
   target: 'static',
   head: {
@@ -72,6 +70,14 @@ export default {
         const fs = require('fs')
         const regex = /```mermaid((.(?!``)|\n)*)```/g
         const matches = file.data.matchAll(regex)
+        const svgMeta = `xmlns:dc="http://purl.org/dc/elements/1.1/"
+                        xmlns:cc="http://creativecommons.org/ns#"
+                        xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+                        xmlns:svg="http://www.w3.org/2000/svg"
+                        xmlns="http://www.w3.org/2000/svg"
+                        xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
+                        xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"`
+
         for (const match of matches) {
           // console.log(`${match[1]}`, file.path)
           // const graphValues = `${match[1]}`
