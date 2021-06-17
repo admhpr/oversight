c85f6d14b058b708723606fb2e06ac4341a8bc3b
admhpr <admhpr@users.noreply.github.com>
2021-06-17 15:47:28 +0000

    Automated deployment: Thu Jun 17 15:47:28 UTC 2021 2e636ee2a27cd02037afbbe207363d92f09a5d04

---
+++ _nuxt/static/1623944838/archive/starting-2019/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1623944838",layout:"default",error:a,serverRendered:true,routePath:"\u002Farchive\u002Fstarting-2019",config:{apiKey:void 0,_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"975a76dd"}}}}(null));
