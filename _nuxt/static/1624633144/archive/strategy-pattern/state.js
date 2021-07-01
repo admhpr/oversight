5f78145f66c9e7c9dec251209ebfbcad29e83b6b
admhpr <admhpr@users.noreply.github.com>
2021-07-01 13:54:04 +0000

    Automated deployment: Thu Jul  1 13:54:04 UTC 2021 3886fbca10405e3f2624f5185656a9fbfc874a9f

--- _nuxt/static/1624633144/archive/strategy-pattern/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1624633144",layout:"default",error:a,serverRendered:true,routePath:"\u002Farchive\u002Fstrategy-pattern",config:{apiKey:void 0,_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"5a0084d4"}}}}(null));
