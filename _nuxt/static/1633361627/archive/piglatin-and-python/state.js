23960d778c813fad0b8828c6f56122c1d2f828cc
admhpr <admhpr@users.noreply.github.com>
2021-10-04 15:33:58 +0000

    Automated deployment: Mon Oct  4 15:33:58 UTC 2021 fa753d850557e426ace0d7d6ab5fb79f0fc96938

---
+++ _nuxt/static/1633361627/archive/piglatin-and-python/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1633361627",layout:"default",error:a,serverRendered:true,routePath:"\u002Farchive\u002Fpiglatin-and-python",config:{apiKey:void 0,_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"a327d0c4"}}}}(null));
