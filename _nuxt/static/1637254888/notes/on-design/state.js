4cec97f0d9eb2db2c0977c432a1497a44c513a55
admhpr <admhpr@users.noreply.github.com>
2021-11-19 15:39:46 +0000

    Automated deployment: Fri Nov 19 15:39:46 UTC 2021 ff654c2842ce4c21bd059545f7e2adeeced71380

--- _nuxt/static/1637254888/notes/on-design/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1637254888",layout:"default",error:a,serverRendered:true,routePath:"\u002Fnotes\u002Fon-design",config:{apiKey:void 0,_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"a2f0cf1b"}}}}(null));
