4bc05b036d2bdb919a4641257dcfe4fb5f9c9c6f
admhpr <admhpr@users.noreply.github.com>
2021-11-29 09:34:25 +0000

    Automated deployment: Mon Nov 29 09:34:24 UTC 2021 bf60cc2a07bf7f681993de50bbd868bb04d5737e

---
+++ _nuxt/static/1638178454/archive/how-flappy-bird-was-made/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1638178454",layout:"default",error:a,serverRendered:true,routePath:"\u002Farchive\u002Fhow-flappy-bird-was-made",config:{apiKey:void 0,_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"206e844d"}}}}(null));
