f3842fbb71a3981b666755df1d08c976c51d53eb
admhpr <admhpr@users.noreply.github.com>
2021-12-03 10:44:26 +0000

    Automated deployment: Fri Dec  3 10:44:26 UTC 2021 1ece1fbd95ed1188f72d549e03548e44deac0257

--- _nuxt/static/1638528204/blog/rust-variables-and-mutability/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1638528204",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Frust-variables-and-mutability",config:{apiKey:void 0,_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"bd4601c6"}}}}(null));
