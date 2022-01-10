ab55a201462f7b0b648838a37f066c98d6bf6daf
admhpr <admhpr@users.noreply.github.com>
2022-01-10 13:55:09 +0000

    Automated deployment: Mon Jan 10 13:55:09 UTC 2022 a6c0ee4cdda234f50263fd319ee9139674a39e4e

--- _nuxt/static/1641822763/archive/adapter-pattern/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1641822763",layout:"default",error:a,serverRendered:true,routePath:"\u002Farchive\u002Fadapter-pattern",config:{apiKey:void 0,_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"32c26378"}}}}(null));
