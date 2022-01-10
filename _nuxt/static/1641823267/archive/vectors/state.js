36e38bedb662e13d7e500890d46ce783567388a8
admhpr <admhpr@users.noreply.github.com>
2022-01-10 14:01:19 +0000

    Automated deployment: Mon Jan 10 14:01:19 UTC 2022 2171c60450e86c4f021ff0068a1beae8fe832a3f

---
+++ _nuxt/static/1641823267/archive/vectors/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1641823267",layout:"default",error:a,serverRendered:true,routePath:"\u002Farchive\u002Fvectors",config:{apiKey:void 0,_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"0b112901"}}}}(null));
