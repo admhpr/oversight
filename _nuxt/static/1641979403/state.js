5cf36cb1a636e5bfa70a9138a1ca55a87e9241f5
admhpr <admhpr@users.noreply.github.com>
2022-01-12 09:28:32 +0000

    Automated deployment: Wed Jan 12 09:28:32 UTC 2022 25266add7f3989cbb2684fe8450a2e2d4a6cd28e

--- _nuxt/static/1641979403/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1641979403",layout:"default",error:a,serverRendered:true,routePath:b,config:{apiKey:void 0,_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"16ed7599"}}}}(null,"\u002F"));
