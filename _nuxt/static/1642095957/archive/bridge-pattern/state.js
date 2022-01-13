263e9d07170e92bc7df74a5f8ae1ff1194e64c74
admhpr <admhpr@users.noreply.github.com>
2022-01-13 17:46:13 +0000

    Automated deployment: Thu Jan 13 17:46:13 UTC 2022 f7752c7e0265556ca4e87e7709b65edf484b5be9

---
+++ _nuxt/static/1642095957/archive/bridge-pattern/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1642095957",layout:"default",error:a,serverRendered:true,routePath:"\u002Farchive\u002Fbridge-pattern",config:{apiKey:void 0,_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"e46032d1"}}}}(null));
---
+++ _nuxt/static/1642095957/blog/rust-variables-and-mutability/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1642095957",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Frust-variables-and-mutability",config:{apiKey:void 0,_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"e46032d1"}}}}(null));
