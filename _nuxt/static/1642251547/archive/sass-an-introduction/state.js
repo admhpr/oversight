cae4a6ef21686b6f3ece71fab58e062ecb4265bb
admhpr <admhpr@users.noreply.github.com>
2022-01-15 13:08:25 +0000

    Automated deployment: Sat Jan 15 13:08:25 UTC 2022 423071b2e0120495e93b6e5fc47b97a884159b0d

--- _nuxt/static/1642251547/archive/sass-an-introduction/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1642251547",layout:"default",error:a,serverRendered:true,routePath:"\u002Farchive\u002Fsass-an-introduction",config:{apiKey:void 0,_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"d2213800"}}}}(null));
