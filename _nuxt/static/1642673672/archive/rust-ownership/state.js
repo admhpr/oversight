338ff3fdb20f4cef007d37bedd30f057f4af5390
admhpr <admhpr@users.noreply.github.com>
2022-01-20 11:39:36 +0000

    Automated deployment: Thu Jan 20 11:39:36 UTC 2022 9955c2303961364208ad68d23e369b69778039c4

--- _nuxt/static/1642673672/archive/rust-ownership/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1642673672",layout:"default",error:a,serverRendered:true,routePath:"\u002Farchive\u002Frust-ownership",config:{apiKey:void 0,_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"01ad2a6b"}}}}(null));
