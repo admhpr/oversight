ae594ebead370cae38c02b4cbe5a29be0f275727
admhpr <admhpr@users.noreply.github.com>
2022-01-20 11:40:39 +0000

    Automated deployment: Thu Jan 20 11:40:39 UTC 2022 02e6c8cc6914daa12e22e8a722e91fcf90e47c19

---
+++ _nuxt/static/1642678829/notes/on-culture/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1642678829",layout:"default",error:a,serverRendered:true,routePath:"\u002Fnotes\u002Fon-culture",config:{apiKey:void 0,_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"1f48bad9"}}}}(null));
