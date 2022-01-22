ec32711d23d3f23fb04c383bf923740ea33392d0
admhpr <admhpr@users.noreply.github.com>
2022-01-22 17:55:30 +0000

    Automated deployment: Sat Jan 22 17:55:30 UTC 2022 f17a0fbbe054493dbd605ca66596f199cd5c4b4e

--- _nuxt/static/1642867596/archive/vectors/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1642867596",layout:"default",error:a,serverRendered:true,routePath:"\u002Farchive\u002Fvectors",config:{apiKey:void 0,_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"57d3eea8"}}}}(null));
---
+++ _nuxt/static/1642874117/archive/simplicity/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1642874117",layout:"default",error:a,serverRendered:true,routePath:"\u002Farchive\u002Fsimplicity",config:{apiKey:void 0,_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"c12c1e7b"}}}}(null));
