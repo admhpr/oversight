b8767703e6202a061ae7bf018b1fbd27189c07a1
admhpr <admhpr@users.noreply.github.com>
2022-01-22 17:56:42 +0000

    Automated deployment: Sat Jan 22 17:56:42 UTC 2022 5ed95fef279c01fdf58fcaa9f2efcfc6679b5002

--- _nuxt/static/1642874117/archive/css-sanity-with-sass/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1642874117",layout:"default",error:a,serverRendered:true,routePath:"\u002Farchive\u002Fcss-sanity-with-sass",config:{apiKey:void 0,_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"c12c1e7b"}}}}(null));
