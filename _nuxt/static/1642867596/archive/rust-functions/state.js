3dcd418788d547adddadd7f273dddc3b70770e20
admhpr <admhpr@users.noreply.github.com>
2022-01-22 16:06:48 +0000

    Automated deployment: Sat Jan 22 16:06:48 UTC 2022 9aaa4f31d3f7bf3127b29058ee9781d7179c14af

---
+++ _nuxt/static/1642867596/archive/rust-functions/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1642867596",layout:"default",error:a,serverRendered:true,routePath:"\u002Farchive\u002Frust-functions",config:{apiKey:void 0,_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"57d3eea8"}}}}(null));
