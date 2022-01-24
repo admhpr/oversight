c30a385af596c5f5ac798acedc8f029dfb689a64
admhpr <admhpr@users.noreply.github.com>
2022-01-24 07:49:51 +0000

    Automated deployment: Mon Jan 24 07:49:51 UTC 2022 23b94cc1031b9a4d58f3ebb04ae97ad1a391dcff

--- _nuxt/static/1643010182/archive/wasm-a-wasm-game-of-life/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1643010182",layout:"default",error:a,serverRendered:true,routePath:"\u002Farchive\u002Fwasm-a-wasm-game-of-life",config:{apiKey:void 0,_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"d05d8558"}}}}(null));
