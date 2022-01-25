ce31c97ebc0dbfa0edc77945257939822f5d36c0
admhpr <admhpr@users.noreply.github.com>
2022-01-25 11:25:14 +0000

    Automated deployment: Tue Jan 25 11:25:14 UTC 2022 06943e47dfb629fe659231ae6e84de21715062ca

---
+++ _nuxt/static/1643109902/archive/facade-pattern/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1643109902",layout:"default",error:a,serverRendered:true,routePath:"\u002Farchive\u002Ffacade-pattern",config:{apiKey:void 0,_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"f3935f4a"}}}}(null));
