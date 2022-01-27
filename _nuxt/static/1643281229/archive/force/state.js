60e52303b46b0fdb92ee513b9774735cdb5047b2
admhpr <admhpr@users.noreply.github.com>
2022-01-27 11:00:40 +0000

    Automated deployment: Thu Jan 27 11:00:40 UTC 2022 67e74d4163f915d33967e1f25772a8c281678c3e

---
+++ _nuxt/static/1643281229/archive/force/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1643281229",layout:"default",error:a,serverRendered:true,routePath:"\u002Farchive\u002Fforce",config:{apiKey:void 0,_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"34b422e4"}}}}(null));
