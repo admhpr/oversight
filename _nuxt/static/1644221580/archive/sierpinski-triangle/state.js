313b721f64cebee7001367044ca59d6664b9a140
admhpr <admhpr@users.noreply.github.com>
2022-02-07 08:17:35 +0000

    Automated deployment: Mon Feb  7 08:17:35 UTC 2022 cbd19e6a5c2f05767eb95e71fe66cd6197ab1fc9

--- _nuxt/static/1644221580/archive/sierpinski-triangle/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1644221580",layout:"default",error:a,serverRendered:true,routePath:"\u002Farchive\u002Fsierpinski-triangle",config:{apiKey:void 0,_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"7c4e120b"}}}}(null));
