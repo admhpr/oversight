7d58dbb0a8c1e6b8b0a8a1e9a5f1c030ee58c115
admhpr <admhpr@users.noreply.github.com>
2022-02-08 09:25:58 +0000

    Automated deployment: Tue Feb  8 09:25:58 UTC 2022 61a8e116f2f1ef1d52670c3473516de73e852a02

--- _nuxt/static/1644312295/archive/state-pattern/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1644312295",layout:"default",error:a,serverRendered:true,routePath:"\u002Farchive\u002Fstate-pattern",config:{apiKey:void 0,_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"6c563639"}}}}(null));
---
+++ _nuxt/static/1644312346/archive/rust-control-flow/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1644312346",layout:"default",error:a,serverRendered:true,routePath:"\u002Farchive\u002Frust-control-flow",config:{apiKey:void 0,_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"b9a62301"}}}}(null));
