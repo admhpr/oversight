915bc2c77637f8b56753fb5c269dd294fae0f69c
admhpr <admhpr@users.noreply.github.com>
2022-02-08 09:22:43 +0000

    Automated deployment: Tue Feb  8 09:22:43 UTC 2022 9f333f54f73abf876d13c6790b6331d97305379d

--- _nuxt/static/1644310758/archive/rust-data-types/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1644310758",layout:"default",error:a,serverRendered:true,routePath:"\u002Farchive\u002Frust-data-types",config:{apiKey:void 0,_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"bfa574a1"}}}}(null));
