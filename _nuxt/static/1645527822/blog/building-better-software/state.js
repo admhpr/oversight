d3dbecc6fc9ecd4ed350075d2882e8180cc0d471
admhpr <admhpr@users.noreply.github.com>
2022-02-22 11:03:54 +0000

    Automated deployment: Tue Feb 22 11:03:54 UTC 2022 10a344b0cb982234bd8060644b828f6f42366f61

---
+++ _nuxt/static/1645527822/blog/building-better-software/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1645527822",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Fbuilding-better-software",config:{apiKey:void 0,_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"972b56f9"}}}}(null));
