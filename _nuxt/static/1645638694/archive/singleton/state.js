57828a82cc2308bd69d63bd3302c3a4a56dda240
admhpr <admhpr@users.noreply.github.com>
2022-02-23 17:51:48 +0000

    Automated deployment: Wed Feb 23 17:51:48 UTC 2022 a86a91a451d587ebf62245d2cf132e23a89ef840

---
+++ _nuxt/static/1645638694/archive/singleton/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1645638694",layout:"default",error:a,serverRendered:true,routePath:"\u002Farchive\u002Fsingleton",config:{apiKey:void 0,_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"ec8912bb"}}}}(null));
