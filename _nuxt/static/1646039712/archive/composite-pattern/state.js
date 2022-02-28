f5e06063adf869412238f358ad93893dfb6903bb
admhpr <admhpr@users.noreply.github.com>
2022-02-28 09:26:23 +0000

    Automated deployment: Mon Feb 28 09:26:23 UTC 2022 eee11b9ae7cdad9b7ce3e0abdf6e627a8bbd2776

--- _nuxt/static/1646039712/archive/composite-pattern/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1646039712",layout:"default",error:a,serverRendered:true,routePath:"\u002Farchive\u002Fcomposite-pattern",config:{apiKey:void 0,_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"c555813a"}}}}(null));
