d52413b1ec0813e0e5f639c72f7e719adb762bec
admhpr <admhpr@users.noreply.github.com>
2022-03-04 10:44:56 +0000

    Automated deployment: Fri Mar  4 10:44:56 UTC 2022 dfa1cf202a5d42b18df5343bd77ed6b1dcbdbbd5

---
+++ _nuxt/static/1646390682/archive/programming-in-the-works-part-1/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1646390682",layout:"default",error:a,serverRendered:true,routePath:"\u002Farchive\u002Fprogramming-in-the-works-part-1",config:{apiKey:void 0,_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"5f107346"}}}}(null));
